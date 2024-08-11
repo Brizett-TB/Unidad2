71c54562e5a75044b11156b54e26a2a0116a2671
Brizett <florenziatrujillo@gmail.com>
2024-08-11 14:10:37 -0500

    adri

---
+++ semana6/grupo/assets/js/script.js
@@ -0,0 +1,27 @@
+// Invertir arreglo
+
+//Contar Vocales
+
+// Función par/impar 
+const comprobar = (n) => {
+    if (n % 2 === 0) {
+        return 0;  // Retorna 0 si el número es par
+    } else {
+        return 1;  // Retorna 1 si el número es impar
+    }
+};
+
+module.exports = comprobar;
+
+//NNúmero mayor que otro 
+const max = (A, B, C) => {
+    if (A >= B && A >= C) {
+        return A;  // Retorna A si es mayor o igual que B y C
+    } else if (B >= A && B >= C) {
+        return B;  // Retorna B si es mayor o igual que A y C
+    } else {
+        return C;  // Retorna C si es mayor o igual que A y B
+    }
+};
+
+module.exports = max;
