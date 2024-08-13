8b8d8e9e37c5086a8755a79d58b3494f48e9e086
Brizett <florenziatrujillo@gmail.com>
2024-08-12 21:15:01 -0500

    avance

---
+++ semana6/s29/assets/js/script.js
@@ -0,0 +1,13 @@
+import {sumar, restar} from "./aritmeticas.js";
+import {multiplicar,dividir} from "./xd.js"
+
+// Usemos la función sumar
+console.log(sumar(15, 34));
+// Usamos la función restar
+console.log(restar(15,76));
+
+// Practica 
+
+console. log(multiplicar(2,24));
+console.log(dividir(30,15));
+
---
+++ semana6/s29/assets/js/xd.js
@@ -0,0 +1,12 @@
+function multiplicar (x, y) {
+    return x*y;
+}
+
+function dividir (x,y) {
+    return x/y;
+}
+
+export {
+    multiplicar,
+    dividir
+};
